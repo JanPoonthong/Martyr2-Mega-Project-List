bbbef177c0908f9816bc1d4f43906e04f6ba33ba
Jan Poonthong <janpoonthong628@gmail.com>
2021-11-24 18:41:41 +0700

    Implement reverse string
    Emoji is not reversing

---
+++ text/reverse-string/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+)
+
+func Reverse(str string) (output string) {
+	for _, char := range str {
+		output = string(char) + output
+	}
+	return
+}
+
+func main() {
+	var userInput string
+	fmt.Print("Enter a word: ")
+	fmt.Scanln(&userInput)
+	fmt.Println(Reverse(userInput))
+}
