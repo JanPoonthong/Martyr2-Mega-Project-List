382527ded66205f162ff5e5b5559b3aac61a6c72
Jan Poonthong <janpoonthong628@gmail.com>
2021-11-26 12:59:22 +0700

    Run gofmt

--- text/reverse-string/main.go
+++ text/reverse-string/main.go
@@ -1,19 +1,34 @@
 package main
 
 import (
-    "fmt"
+	"bufio"
+	"fmt"
+	"log"
+	"os"
 )
 
-func Reverse(str string) (output string) {
-    for _, char := range str {
-        output = string(char) + output
-    }
-    return
+func Reverse(input string) (output string) {
+	n := 0
+	rune := make([]rune, len(input))
+	for _, r := range input {
+		rune[n] = r
+		n++
+	}
+	rune = rune[0:n]
+
+	for i := 0; i < n/2; i++ {
+		rune[i], rune[n-1-i] = rune[n-1-i], rune[i]
+	}
+
+	output = string(rune)
+	return
 }
 
 func main() {
-    var userInput string
-    fmt.Print("Enter a word: ")
-    fmt.Scanln(&userInput)
-    fmt.Println(Reverse(userInput))
+	in := bufio.NewReader(os.Stdin)
+	if line, err := in.ReadString('\n'); err != nil {
+		log.Fatal(err)
+	} else {
+		fmt.Println(Reverse(line))
+	}
 }
